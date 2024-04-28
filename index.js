ae282aadde711b96bea7e09d5c003435895de5ce
shahinexy <shahinexy@gmail.com>
2024-04-28 10:06:41 +0600

    get single data by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,31 +29,37 @@ async function run() {
 
         const artAndcraftCollection = client.db("art&craftDB").collection("art&craft");
 
-        app.post('/art&craft', async(req, res)=>{
+        app.post('/art&craft', async (req, res) => {
             const newItem = req.body;
-            console.log(newItem);
             const result = await artAndcraftCollection.insertOne(newItem)
             res.send(result)
         })
 
-        app.get('/art&craft', async(req, res)=>{
+        app.get('/art&craft', async (req, res) => {
             const cursor = artAndcraftCollection.find()
             const result = await cursor.toArray()
             res.send(result)
         })
 
-        app.get('/art&craft/:loginEmail', async(req, res)=>{
+        app.get('/art&craft/:loginEmail', async (req, res) => {
             const email = req.params.loginEmail;
-            console.log(email);
-            const filter = {loginEmail: email};
+            const filter = { loginEmail: email };
             const result = await artAndcraftCollection.find(filter).toArray();
+            res.send(result)
+        })
+
+        app.get('/art&craft/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const quary = { _id: new ObjectId(id) };
+            const result = await artAndcraftCollection.findOne(quary);
             res.send(result)
         })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        
+
     }
 }
 run().catch(console.dir);
